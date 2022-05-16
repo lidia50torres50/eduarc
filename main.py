829fba70ace59f0bb581821db25bb61c842a02f8
lidia50torres50 <103346724+lidia50torres50@users.noreply.github.com>
2022-05-15 21:10:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'CarlosAle0077'
+        tl_admin_user = 'yunior132001'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -604,7 +604,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 18693993
             #    api_hash = '382ee6b53bdd0df66a52ea9779c62424'
-            #    bot_token = '5300793354:AAHng0VyvI6joUWiYWzy7trdgjT7rVBZgBA'
+            #    bot_token = '5371733981:AAF-C9H4xrVMqHYgFdFHXxqyVFkvY99Sdrw'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -620,7 +620,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5300793354:AAHng0VyvI6joUWiYWzy7trdgjT7rVBZgBA'
+    bot_token = '5371733981:AAF-C9H4xrVMqHYgFdFHXxqyVFkvY99Sdrw'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
